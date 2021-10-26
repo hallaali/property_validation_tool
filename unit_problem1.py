67a2ea93810c51738bd340ebb90841fc60ff883b
hallaali <77864659+hallaali@users.noreply.github.com>
2021-10-26 15:08:41 -0400

    Update unit_problem1.py

--- unit_problem1.py
+++ unit_problem1.py
@@ -1,1 +1,47 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+NavigationToolbar2Tk)
 
+
+
+
+# The main tkinter window
+window = Tk()
+window.geometry("500x200")
+# setting the title
+window.title('Property Package Validation')
+
+title1 = Label(window, text="Pratt & Whitney")
+title1.grid(row=0,column=0)
+
+def enthalpy_h():
+      fig = plt.figure()
+      plot1 = fig.add_subplot(111)
+      x = df['T_val']
+      y = df['P_val']
+      plot1.plot(x, y, marker='o', color='green')
+      plt.show()
+
+
+def getFile():
+      global df
+      import_file_path = filedialog.askopenfilename()
+      headers = ['P_val', 'T_val', 'h_val_CEA', 's_val_CEA', 'k_val_CEA', 'mu_val_CEA', 'Cp_val_CEA', 'gam_val_CEA', 'rho_val_CEA', 'R_val_CEA', 'h_val_pkg', 's_val_pkg', 'k_val_pkg', 'mu_val_pkg', 'Cp_val_pkg', 'gam_val_pkg', 'rho_val_pkg', 'R_val_pkg', 'h_val_err', 's_val_err', 'k_val_err', 'mu_val_err', 'Cp_val_err', 'gam_val_err', 'rho_val_err', 'R_val_err', 'outlier_val']
+      df = pd.read_csv(import_file_path, skiprows=1, usecols=headers, sep=' ')
+
+
+browseButton = tk.Button(text='Load File...', command=getFile)
+browseButton.grid(row=20, column=0)
+
+plotButton = tk.Button(text='Plot', command=enthalpy_h)
+plotButton.grid(row=20, column=2)
+
+window.mainloop()
