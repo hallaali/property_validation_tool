0d06008e9bad390f706c72dd70cf7be111a8e573
hallaali <77864659+hallaali@users.noreply.github.com>
2021-10-26 14:35:18 -0400

    Update unit_problem.py

--- unit_problem.py
+++ unit_problem.py
@@ -22,13 +22,29 @@ window.title('Property Package Validation')
 title1 = Label(window, text="Pratt & Whitney")
 title1.grid(row=0,column=0)
 
+def enthalpy_h():
+      fig = plt.figure()
+      plot1 = fig.add_subplot(111)
+      x = df['T']
+      y_pkg = df['h_pkg']
+      y_CEA = df['h_CEA']
+      plot1.plot(x, y_pkg, marker='o', color='green')
+      plot1.plot(x, y_CEA, color='blue')
+      plt.show()
+
 
 def getFile():
       global df
       import_file_path = filedialog.askopenfilename()
-      df = pd.read(import_file_path)
+      headers = ['T', 'h_pkg', 'h_CEA']
+      df = pd.read_csv(import_file_path, usecols=headers, sep='\t')
+      x = df['T']
+      y = df['h_pkg']
 
 browseButton = tk.Button(text='Load File...', command=getFile)
-browseButton.grid(row=20, column=2)
+browseButton.grid(row=20, column=0)
+
+plotButton = tk.Button(text='Plot', command=enthalpy_h)
+plotButton.grid(row=20, column=2)
 
 window.mainloop()
